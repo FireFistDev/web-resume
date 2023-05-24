a3c9b0a46ea56fe725921d625463515851a4d759
FireFistDev <akaki7600@gmail.com>
2023-05-24 13:44:32 +0400

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,28 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
-
+import firkify from '../assets/projects/Forkify.png'
+import milionare from '../assets/projects/milionare.png'
+import socialNetwork from '../assets/projects/Screenshot 2023-05-24 133440.png'
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"SocialNetwork app based on MERN STACK",
+        image:socialNetwork,
+        github:"https://github.com/FireFistDev/SocialNetwork",
+        live:"https://main--cozy-lily-e0594b.netlify.app/",
+    },
+
+    {
+        id:1,
+        name:"Vinila JS App Firkify",
+        image:firkify,
+        github:"https://github.com/FireFistDev/forkify-web-app",
+        live:"https://forkify-firefist.netlify.app/",
+    },
+    {
+        id:1,
+        name:"React app Become Milionre",
+        image:milionare,
+        github:"https://github.com/FireFistDev/become-milionare",
+        live:"https://become-milionare.netlify.app/",
     }
 
 ]
